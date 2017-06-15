4c8dd8f51e6d5235ef867537a349f3830eed0611
GingiWang <pokitimmy@hotmail.com>
2017-06-15 15:47:20 +0800

    20170615

---
+++ SuperUniversity/Areas/Courses/Controllers/CoursesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Net;
+using MvcClient.Areas.Courses.Models;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace MvcClient.Areas.Courses.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses/Courses
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public static String domain = "http://taiwanuniversitiescourses.azurewebsites.net/";
+        public static String queryString = null;
+
+
+        public async Task<ActionResult> GetNtuBySearchAll(string query = null)
+        {
+            IEnumerable<NtuCourseModel> ntuCourses = await CoursesControllerUtl.BySearchAllWholeWork<NtuCourseModel>(domain, "ntu", query);
+            return PartialView(ntuCourses);
+        }
+
+
+        public async Task<ActionResult> GetNtpuBySearchAll(string query = null)
+        {
+            IEnumerable<NtpuCourseModel> ntpuCourses = await CoursesControllerUtl.BySearchAllWholeWork<NtpuCourseModel>(domain, "ntpu", query);
+            return PartialView(ntpuCourses);
+        }
+
+
+        public async Task<ActionResult> GetNctuBySearchAll(string query = null)
+        {
+            IEnumerable<NctuCourseModel> nctuCourses = await CoursesControllerUtl.BySearchAllWholeWork<NctuCourseModel>(domain, "nctu", query);
+            return PartialView(nctuCourses);
+        }
+
+
+        public async Task<ActionResult> GetNckuBySearchAll(string query = null)
+        {
+            IEnumerable<NckuCourseModel> nckuCourses = await CoursesControllerUtl.BySearchAllWholeWork<NckuCourseModel>(domain, "ncku", query);
+            return PartialView(nckuCourses);
+        }
+
+        //[ChildActionOnly]
+        public async Task<ActionResult> GetBySearchAll(string college = "NTU", string query = null)
+        {
+            IEnumerable<AllCollegeCourseModel> AllCollegeCourses = await CoursesControllerUtl.BySearchAllWholeWorkForAll<AllCollegeCourseModel>(domain, "AllCollege", college, query);
+            return PartialView(AllCollegeCourses);
+        }
+
+        public async Task<ActionResult> GetCourseDetail(string strid)
+        {
+            AllCollegeCourseModel SingleCourse = await CoursesControllerUtl.ByIdWholeWorkForAll<AllCollegeCourseModel>(domain, "AllCollege", strid);
+            return View(SingleCourse);
+
+        }
+
+        public async Task<ActionResult> GetComments(string strid)
+        {
+            AllCollegeCourseModel SingleCourse = await CoursesControllerUtl.ByIdWholeWorkForAll<AllCollegeCourseModel>(domain, "AllCollege", strid);
+            List<Comment> comments = SingleCourse.commentdata;
+            return PartialView(comments);
+        }
+
+        public async Task<ActionResult> GetRankings(string strid)
+        {
+            AllCollegeCourseModel SingleCourse = await CoursesControllerUtl.ByIdWholeWorkForAll<AllCollegeCourseModel>(domain, "AllCollege", strid);
+            List<Ranking> rankings = SingleCourse.rankingdata;
+            return PartialView(rankings);
+        }
+
+
+        [HttpGet]
+        public ActionResult PostComment()
+        {
+            return PartialView();
+        }
+
+
+        [HttpGet]
+        public ActionResult PostRanking()
+        {
+            return PartialView();
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> PostComment(string strid, Comment comment)
+        {
+            AllCollegeCourseModel SingleCourse = await CoursesControllerUtl.ByIdWholeWorkForAll<AllCollegeCourseModel>(domain, "AllCollege", strid);
+            return PartialView(SingleCourse);
+        }
+
+
+
+        [HttpPost]
+        public async Task<ActionResult> PostRanking(string strid, Ranking ranking)
+        {
+            AllCollegeCourseModel SingleCourse = await CoursesControllerUtl.ByIdWholeWorkForAll<AllCollegeCourseModel>(domain, "AllCollege", strid);
+            return PartialView(SingleCourse);
+        }
+
+
+
+
+    }
+}
---
+++ SuperUniversity/Areas/Courses/Models/AllCollegeCourseModel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcClient.Areas.Courses.Models
+{
+    public class AllCollegeCourseModel
+    {
+        //[HiddenInput(DisplayValue =false)]
+        public string _id { get; set; }
+        public string College { get; set; }
+        [DisplayName("課號")]
+        public string Identity { get; set; }
+        [DisplayName("最後更新學期")]
+        public string Semester { get; set; }
+        [DisplayName("課程名稱")]
+        public string CourseName { get; set; }
+        [DisplayName("老師")]
+        public string Teacher { get; set; }
+        [DisplayName("時間教室")]
+        public string TimeAndClassroom { get; set; }
+        [DisplayName("開課系所")]
+        public string Major { get; set; }
+        [DisplayName("必修")]
+        public string Required { get; set; }
+        [DisplayName("人數上限")]
+        public string MaxNum { get; set; }
+        [DisplayName("學分")]
+        public string Credit { get; set; }
+        [DisplayName("修課限制")]
+        public string Limitation { get; set; }
+        [DisplayName("備註")]
+        public string Note { get; set; }
+
+        public List<Comment> commentdata { get; set; }
+        public List<Ranking> rankingdata { get; set; }
+        public DateTime lastModified { get; set; }
+    }
+
+
+    public class Comment
+    {
+
+        [DisplayName("姓名")]
+        [Required(ErrorMessage = "請輸入姓名")]
+        [DataType(DataType.Text)]
+        public string name { get; set; }
+
+        [DisplayName("電子郵件")]
+        [EmailAddress(ErrorMessage = "不符合電子郵件格式")]
+        [Required(ErrorMessage = "請輸入電子郵件")]
+        [DataType(DataType.EmailAddress)]
+        public string email { get; set; }
+
+        [DisplayName("評論內容")]
+        [Required(ErrorMessage = "請輸入留言內容")]
+        [DataType(DataType.MultilineText)]
+        public string commentstring { get; set; }
+
+        [DisplayName("匿名留言")]
+        public bool anonym { get; set; }
+
+        public DateTime CommentTime { get; set; }
+    }
+
+    public class Ranking
+    {
+        [DisplayName("深度")]
+        [Range(1, 10, ErrorMessage = "必須介在1到10之間")]
+        public int deepness { get; set; }
+
+        [DisplayName("涼度")]
+        [Range(1, 10, ErrorMessage = "必須介在1到10之間")]
+        public int relaxing { get; set; }
+
+        [DisplayName("甜度")]
+        [Range(1, 10, ErrorMessage = "必須介在1到10之間")]
+        public int sweetness { get; set; }
+
+        public DateTime RankTime { get; set; }
+    }
+}
